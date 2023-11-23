091b5fdb50611cfa46f5ae80884a1f384a9fb9ef
BeAware-2002 <manjireepatil0008@gmail.com>
2023-11-23 23:08:51 +0530

    commit 9

---
+++ TNS_Project/src/mypack/CollectionStack.java
@@ -0,0 +1,17 @@
+package mypack;
+import java.io.*;
+import java.util.*;
+public class CollectionStack {
+	Stack<String> stack = new Stack<String>();  
+	stack.push("Ayush");  
+	stack.push("Garvit");  
+	stack.push("Amit");  
+	stack.push("Ashish");  
+	stack.push("Garima");  
+	stack.pop();  
+	Iterator<String> itr=stack.iterator();  
+	while(itr.hasNext()){  
+	System.out.println(itr.next());  
+	}  
+	}  
+}
